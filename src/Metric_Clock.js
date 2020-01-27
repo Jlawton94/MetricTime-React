78fb961b04c2aedeaa1c2c6a84a9a3c76776b0e4
Jacob Lawton <jacob.lawton@egym.de>
2020-01-27 16:08:20 +0100

    react set up

---
+++ src/Metric_Clock.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Metric_Clock.css';
+
+function Metric_Clock() {
+  return (
+    <div className="App">
+        Jacob
+    </div>
+  );
+}
+
+export default Metric_Clock;
